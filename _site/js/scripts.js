2d60f574b5f3b2e2bc18120cdf9d244afd6f1c33
itsguus <itsguus@gmail.com>
2020-06-30 17:17:37 +0200

    update Guus

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -10,11 +10,41 @@ function updateTime() {
         hours = document.querySelector(".hours p:first-of-type"),
         minutes = document.querySelector(".minutes p:first-of-type"),
         seconds = document.querySelector(".seconds p:first-of-type");
-        days.textContent = Difference_In_Days;
-        hours.textContent = Difference_In_Hours;
-        minutes.textContent = Difference_In_Minutes;
-        seconds.textContent = Difference_In_Seconds;
+    days.textContent = Difference_In_Days;
+    hours.textContent = Difference_In_Hours;
+    minutes.textContent = Difference_In_Minutes;
+    seconds.textContent = Difference_In_Seconds;
 }
 updateTime();
 
-setInterval(updateTime,1000); 
+setInterval(updateTime, 1000);
+
+function hetIsBal() {
+    document.querySelector(".overlay").classList.toggle("displayNone");
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function flyNames() {
+    var allNames = document.querySelectorAll(".overlay h1"),
+        i;
+    for (i = 0; i < allNames.length; i++) {
+        var randh = Math.ceil(Math.random() * 93 ),
+        randv = Math.ceil(Math.random() * 97 ),
+        randColor = getRandomColor(),
+        randAng = Math.ceil(Math.random() * 360 );
+
+        
+        allNames[i].style="top: " + randh + "%; left: " + randv + "%; color: " + randColor + "; transform: rotate(" + randAng +"deg);"; 
+    }
+}
+setInterval(flyNames, 5000)
+
+flyNames();
